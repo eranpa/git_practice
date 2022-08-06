d5f3ba9a167848398263d56f27ba65a65409b4c6
David Eldar <doomtak@gmail.com>
2022-08-06 21:50:39 +0300

    mashu

---
+++ git_firebase/src/views/home-page.vue
@@ -0,0 +1,18 @@
+ <template>
+    <div>
+        <h1>hello branch</h1>
+    </div>
+ </template>
+
+ <script>
+ export default {
+
+ data() {
+ return {};
+ },
+ created() {},
+ methods: {},
+ computed: {},
+ unmounted() {},
+ };
+ </script>
